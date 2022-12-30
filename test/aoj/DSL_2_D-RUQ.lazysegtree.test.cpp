4e9fefb57f9a148fc2888172243e418718302e87
m3h3d1 <hasanmehedi185@gmail.com>
2022-12-31 05:10:12 +0600

    Update DSL_2_D-RUQ.lazysegtree.test.cpp

--- test/aoj/DSL_2_D-RUQ.lazysegtree.test.cpp
+++ test/aoj/DSL_2_D-RUQ.lazysegtree.test.cpp
@@ -1,4 +1,4 @@
-#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/3/DSL/2/DSL_2_B"
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/3/DSL/2/DSL_2_D"
 #include <bits/stdc++.h>
 using namespace std;
 #include "../../segtree/lazysegtree.hpp"
