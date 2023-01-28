9c65425825895e48a885f8f748d799e2d005030b
m3h3d1 <hasanmehedi185@gmail.com>
2023-01-28 23:01:04 +0600

    Update union_find.hpp

--- ds/union_find.hpp
+++ ds/union_find.hpp
@@ -1,18 +1,22 @@
 struct DSU {
-	vector<int> e;
-	DSU(int n) { e = vector<int>(n, -1); }
+    vector<int> e;
+    int cc; // connected components
+    DSU(int n): e(n, -1), cc(n) { }
 
-	// get representive component (uses path compression)
-	int get(int x) { return e[x] < 0 ? x : e[x] = get(e[x]); }
+    // get representive component (uses path compression)
+    int get(int x) { return e[x] < 0 ? x : e[x] = get(e[x]); }
 
-	bool same_set(int a, int b) { return get(a) == get(b); }
+    bool same_set(int a, int b) { return get(a) == get(b); }
 
-	int size(int x) { return -e[get(x)]; }
+    int size(int x) { return -e[get(x)]; }
 
-	bool unite(int x, int y) {  // union by size
-		x = get(x), y = get(y);
-		if (x == y) return false;
-		if (e[x] > e[y]) swap(x, y);
-		e[x] += e[y]; e[y] = x; return true;
-	}
+    int groups() { return cc; } // number of groups
+
+    bool unite(int x, int y) {  // union by size
+        x = get(x), y = get(y);
+        if (x == y) return false;
+        cc--;
+        if (e[x] > e[y]) swap(x, y);
+        e[x] += e[y]; e[y] = x; return true;
+    }
 };
