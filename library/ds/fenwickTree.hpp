713326e54d5f9ca9eff414ed3be1c2c73fbf2f98
G M Mehedi Hasan <39239695+m3h3d1@users.noreply.github.com>
2024-03-16 05:38:10 +0600

    fenwick tree document update

--- library/ds/fenwickTree.hpp
+++ library/ds/fenwickTree.hpp
@@ -6,7 +6,8 @@ template <class T> struct FenwickTree { // 1 indexed
     FenwickTree() : n(0) {}
     FenwickTree(int n) : n(n), data(n+1, 0) {}
 
-    // a[id] += x
+    // a[id] += x and all the ranges responsible for 'id', O(log(n))
+    // x = i&-i => lsb(x); lsb(0b1101000) = 0b1000 = 8
     void add(int id, T x) {
         for (int i = id; i <= n; i += i & -i) {
             data[i] += x;
@@ -17,7 +18,7 @@ template <class T> struct FenwickTree { // 1 indexed
         add(r + 1, -val);
     }
 
-    // [1, r]
+    // prefix sum from [1, r], O(log(n))
     T sum(int r) {
         T s(0);
         for (int i = r; i > 0; i -= i & -i) {
--- library/ds/fenwickTree.md
+++ library/ds/fenwickTree.md
@@ -7,7 +7,8 @@ documentation_of: ./fenwickTree.hpp
 
 
 
-## Resoources
+## Resources
 
-- https://cp-algorithms.com/data_structures/fenwick.html#2-range-update-and-point-query
-- https://www.youtube.com/watch?v=RgITNht_f4Q&list=PLDV1Zeh2NRsB6SWUrDFW2RmDotAfPbeHu&index=38
+- [cp-algorithms](https://cp-algorithms.com/data_structures/fenwick.html)
+- [Youtube - WilliamFiset](https://www.youtube.com/watch?v=RgITNht_f4Q&list=PLDV1Zeh2NRsB6SWUrDFW2RmDotAfPbeHu&index=38)
+- https://nyaannyaan.github.io/library/data-structure/binary-indexed-tree.hpp
